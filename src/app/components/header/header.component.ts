b4deae0038ce46e006136a4671cae6992b296ada
SantiagoTonda <superorbe@gmail.com>
2022-11-05 02:25:56 -0300

    El Ale lo confirmó

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,7 +11,7 @@ import { Router } from "@angular/router";
 export class HeaderComponent implements OnInit {
 
   title:string = 'My TaskList';
-  showAddTask: boolean = true;
+  showAddTask: boolean = false;
   subscription?: Subscription
 
   constructor(
